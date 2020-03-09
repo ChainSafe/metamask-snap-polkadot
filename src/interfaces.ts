04fc59256c7de018ecbb80cb1b1b36e7e10b80e2
Marin Petrunić <marin.petrunic@gmail.com>
2020-03-09 09:17:42 +0100

    add eslint

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,9 +1,9 @@
 export type FMethodCallback = (
-    originString: string,
-    requestObject: { method: string; params: any }
-) => Promise<any>;
+  originString: string,
+  requestObject: { method: string; params: unknown }
+) => Promise<unknown>;
 
 export interface Wallet {
-    registerRpcMessageHandler: (fn: FMethodCallback) => any;
-    send(options: {method: string, params: any[]}): void;
+  registerRpcMessageHandler: (fn: FMethodCallback) => unknown;
+  send(options: {method: string; params: unknown[]}): void;
 }
