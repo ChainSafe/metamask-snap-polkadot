ced5238a0a3f879ba2a6e8a3ff769e76f53ad52b
Marin Petrunić <marin.petrunic@gmail.com>
2020-03-16 20:27:30 +0100

    add react example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Metamask} from "./metamask";
 
 function App() {
+
+    const [metamask, setMetamask] = useState<Metamask>(new Metamask());
+
+    const initMetamask = useCallback(async () => {
+        const initialized = await metamask.init();
+        if(initialized) {
+            setMetamask(metamask);
+        } else {
+            alert("Failed to initialize plugin");
+        }
+    }, [metamask]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button onClick={initMetamask}>Connect</button>
       </header>
     </div>
   );
