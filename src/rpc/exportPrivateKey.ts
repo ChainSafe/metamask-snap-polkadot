bf1cfd77f693813363d04654c03256601cb656bc
mak <mak@nodefactory.io>
2020-03-19 14:56:15 +0100

    Move confirmation to separate function

--- src/rpc/exportPrivateKey.ts
+++ src/rpc/exportPrivateKey.ts
@@ -1,16 +1,17 @@
 import {Wallet} from "../interfaces";
 import {toHexString} from "../util/hex";
+import {showConfirmationDialog} from "../util/confirmation";
 
 export async function exportPrivateKey(wallet: Wallet): Promise<string> {
   const keyPairState = wallet.getPluginState();
   if (keyPairState != null) {
     // ask for confirmation
-    const confirmation = await wallet.send({
-      method: 'confirm',
-      params: ['Do you want to export your private key?']
-    });
+    const confirmation = await showConfirmationDialog(
+      wallet,
+      'Do you want to export your private key?'
+    );
     // return private key
-    if (confirmation.result == true) {
+    if (confirmation) {
       // keypair already saved
       const sk = keyPairState.polkadot.account.secretKey;
       return toHexString(sk);
