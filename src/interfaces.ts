2eb5d696768ad250dd6da5eaee11dca026fbc503
mak <mak@nodefactory.io>
2020-03-25 13:36:35 +0100

    Fix index page, fix export private key and fix conformation dialog

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -8,11 +8,9 @@ export type RequestObject = { method: string; params: unknown };
 export type KeyPairState = { secretKey: Uint8Array; publicKey: Uint8Array };
 export type MetamaskState = {polkadot: {account: KeyPairState}};
 
-export type RpcResponse = {id: number; jsonrpc: string; result: unknown; error: unknown};
-
 export interface Wallet {
   registerRpcMessageHandler: (fn: FMethodCallback) => unknown;
-  send(options: {method: string; params: unknown[]}): RpcResponse;
+  send(options: {method: string; params: unknown[]}): unknown;
   getAppKey(): string;
   updatePluginState(state: MetamaskState): void;
   getPluginState(): MetamaskState;
