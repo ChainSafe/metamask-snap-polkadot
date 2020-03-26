4182ccb58a2ca054b48c9f2de95fe25cb5a93f32
mak <mak@nodefactory.io>
2020-03-26 09:46:11 +0100

    Implement getAddress function

--- src/rpc/getAddress.ts
+++ src/rpc/getAddress.ts
@@ -1,5 +1,9 @@
 import {Wallet} from "../interfaces";
 
 export async function getAddress(wallet: Wallet): Promise<string> {
-  return "address";
+  const state = wallet.getPluginState();
+  if (state != null) {
+    return state.polkadot.account.keyring.address;
+  }
+  return null;
 }
