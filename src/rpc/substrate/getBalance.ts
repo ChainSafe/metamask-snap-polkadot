ba22f8f0729750ffe5359c3d2ca45dc678676c47
mak <mak@nodefactory.io>
2020-03-26 11:05:13 +0100

    Implement getBalance function

--- src/rpc/substrate/getBalance.ts
+++ src/rpc/substrate/getBalance.ts
@@ -1,5 +1,12 @@
 import {Wallet} from "../../interfaces";
+import {initApi} from "../../polkadot/initApi";
 
 export async function getBalance(wallet: Wallet): Promise<string> {
-  return "120";
+  const api = await initApi();
+  const state = wallet.getPluginState();
+  if (state != null) {
+    const account = await api.query.system.account(state.polkadot.account.keyring.address);
+    return account.data.free.toHuman();
+  }
+  return null;
 }
