68c6f716e795032869f721ea189f6ba2e4921c1c
mak <mak@nodefactory.io>
2020-03-27 09:49:25 +0100

    Move url to const

--- src/rpc/substrate/getTransactions.ts
+++ src/rpc/substrate/getTransactions.ts
@@ -2,13 +2,15 @@ import {Wallet} from "../../interfaces";
 import ApiPromise from "@polkadot/api/promise";
 import axios from "axios";
 
+const API_PATH = "https://api-01.polkascan.io/kusama/api/v1/balances/transfer";
+
 export async function getTransactions(wallet: Wallet, api: ApiPromise): Promise<string> {
   const state = wallet.getPluginState();
   if (state != null) {
     // eslint-disable-next-line max-len
-    const response = await axios.get(`https://api-01.polkascan.io/kusama/api/v1/balances/transfer?&filter[address]=${state.polkadot.account.keyring.address}`);
-    // const account = await api.query.system.account(state.polkadot.account.keyring.address);
-    // return account.data.free.toHuman();
+    const response = await axios.get(`${API_PATH}?&filter[address]=${state.polkadot.account.keyring.address}`);
+
+
   }
   return null;
 }
