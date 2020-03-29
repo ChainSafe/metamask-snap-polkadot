e9a60134e466dc12f1fe3132a45472e25ce23bdf
Marin Petrunić <marin.petrunic@gmail.com>
2020-03-29 22:59:58 +0200

    add metamask connection logic and context

---
+++ example/src/services/metamask.ts
@@ -0,0 +1,34 @@
+declare global {
+    interface Window {
+        ethereum: {
+            isMetaMask: boolean;
+            send: (params: any)=> Promise<{error: any}&any[]>
+        }
+    }
+}
+
+export function hasMetaMask(): boolean {
+    if (!window.ethereum) {
+        return false
+    }
+    return window.ethereum.isMetaMask;
+
+}
+
+export async function installPolkadotSnap(): Promise<boolean> {
+    const origin = new URL('package.json', 'http://localhost:8081').toString();
+    const pluginOrigin = `wallet_plugin_${origin}`;
+    try {
+        await window.ethereum.send({
+            method: 'wallet_enable',
+            params: [{
+                [pluginOrigin]: {}
+            }]
+        });
+        console.log("Snap installed!!");
+        return true;
+    } catch (e) {
+        console.log(e);
+        return false;
+    }
+}
