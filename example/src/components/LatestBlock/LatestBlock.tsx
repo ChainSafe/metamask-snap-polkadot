0061524554324d1fa4666b05d126ad63475c931c
mak <mak@nodefactory.io>
2020-03-31 17:32:35 +0200

    Fetch latest block on interval

--- example/src/components/LatestBlock/LatestBlock.tsx
+++ example/src/components/LatestBlock/LatestBlock.tsx
@@ -1,7 +1,19 @@
 import {useEffect, useState} from "react";
+import {getBalance} from "../../services/account";
+import {getLatestBlock} from "../../services/block";
 
 export const LatestBlock = () => {
     let [latestBlock, setLatestBlock] = useState("");
 
-    useEffect()
+    useEffect(() => {
+        // fetch balance every 3 second
+        const interval = setInterval(async () => {
+            setLatestBlock(await getLatestBlock())
+        }, 3000);
+        return function cleanup() {
+            if (interval) {
+                clearInterval(interval)
+            }
+        };
+    }, []);
 }
