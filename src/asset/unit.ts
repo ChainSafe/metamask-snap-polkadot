a2c9f2e4633ea8aee1a7f31fe1fe9caf6232a584
Marin Petrunić <marin.petrunic@gmail.com>
2020-03-31 21:50:09 +0200

    fix lint

--- src/asset/unit.ts
+++ src/asset/unit.ts
@@ -3,9 +3,10 @@ import ApiPromise from "@polkadot/api/promise";
 import {getBalance} from "../rpc/substrate/getBalance";
 import {getAddress} from "../rpc/getAddress";
 import {executeAssetOperation} from "./executeAssetOperation";
-import formatBalance from "@polkadot/util/format/formatBalance"
+import formatBalance from "@polkadot/util/format/formatBalance";
+import {Balance} from "@polkadot/types/interfaces";
 
-export function getPolkadotAssetDescription(balance: any, address: string): Asset {
+export function getPolkadotAssetDescription(balance: number|string|Balance, address: string): Asset {
   return {
     balance: formatBalance(balance, {decimals: 12, withSi: true, withUnit: false}),
     customViewUrl: `https://polkascan.io/pre/kusama/account/${address}`,
@@ -19,8 +20,8 @@ export function getPolkadotAssetDescription(balance: any, address: string): Asse
 export async function createPolkadotAsset(wallet: Wallet, api: ApiPromise, method: "update" | "add"): Promise<Asset> {
 
   const [balance, address] = await Promise.all([
-      getBalance(wallet, api),
-      getAddress(wallet)
+    getBalance(wallet, api),
+    getAddress(wallet)
   ]);
   const asset = getPolkadotAssetDescription(balance, address);
   // remove asset if already created
--- src/rpc/substrate/getBalance.ts
+++ src/rpc/substrate/getBalance.ts
@@ -1,6 +1,7 @@
 import {Wallet} from "../../interfaces";
 import ApiPromise from "@polkadot/api/promise";
 import {getKeyPair} from "../../polkadot/account";
+import {Balance} from "@polkadot/types/interfaces";
 
 /**
  * Returns balance as BN
@@ -8,7 +9,7 @@ import {getKeyPair} from "../../polkadot/account";
  * @param api
  * @param address
  */
-export async function getBalance(wallet: Wallet, api: ApiPromise, address?: string): Promise<any> {
+export async function getBalance(wallet: Wallet, api: ApiPromise, address?: string): Promise<Balance> {
   if(!address) {
     address = (await getKeyPair(wallet)).address;
   }
