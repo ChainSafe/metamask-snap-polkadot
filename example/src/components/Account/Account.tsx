5ee1c7e1c07dfd21f681e44bc64282b4ce803b2e
irubido <ivan.rubido@gmail.com>
2020-04-03 13:51:12 +0200

    display private key

--- example/src/components/Account/Account.tsx
+++ example/src/components/Account/Account.tsx
@@ -27,6 +27,11 @@ export const Account = () => {
     //     };
     // }, []);
 
+    const handleExport = async () => {
+        const privateKey = await exportSeed();
+        alert(privateKey);
+    }
+
     return (
         <Card>
             <CardHeader title="Account details"/>
@@ -48,7 +53,7 @@ export const Account = () => {
                     </Grid>
                 </Grid>
                 <Grid container item xs={12} justify="flex-end">
-                    <Button color="secondary" variant={"contained"} onClick={exportSeed}>Export private key</Button>
+                    <Button color="secondary" variant={"contained"} onClick={handleExport}>Export private key</Button>
                 </Grid>
             </CardContent>
         </Card>
