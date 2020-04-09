dfe5340444fd21715bcdd51d9fc7a0bf6cef1826
irubido <ivan.rubido@gmail.com>
2020-04-09 13:40:44 +0200

    code fix

--- adapter/index.ts
+++ adapter/index.ts
@@ -1,8 +1,6 @@
 import { injectExtension } from '@polkadot/extension-inject';
-import {Injected, InjectedAccounts} from "@polkadot/extension-inject/types";
+import {Injected, InjectedAccounts, InjectedAccount} from "@polkadot/extension-inject/types";
 import { Signer as InjectedSigner } from '@polkadot/api/types';
-import {installPolkadotSnap, isPolkadotSnapInstalled} from "../example/src/services/metamask";
-import {pluginOrigin} from "../example/src/services/metamask";
 
 interface NetworkConfig {
     explorerUrl: string;
@@ -11,10 +9,20 @@ interface NetworkConfig {
 }
 
 class MetamaskPolkadotSnap implements Injected {
-    //TODO
+
     public accounts: InjectedAccounts = {
-        get: ()=>{},
-        subscribe: ()=>{}
+        get: async () => {
+            const account: InjectedAccount = {
+               address: await getAccountAddress(),
+               genesisHash: null,
+               name: "Metamask account"
+            }
+            return [account];
+            },
+        subscribe: () => {
+            const unsubCall = () => {};
+            return unsubCall;
+            }
     }
     public signer: InjectedSigner;
     
@@ -24,24 +32,91 @@ class MetamaskPolkadotSnap implements Injected {
     };
 
 
-    public enableSnap = async (origin: string): Promise<Injected> => {
+    public enableSnap = async (origin?: string): Promise<Injected> => {
+        const defaultOrigin = new URL('package.json', 'http://localhost:8081').toString();
+        const defaultPluginOrigin = `wallet_plugin_${defaultOrigin}`;
+
         if(isPolkadotSnapInstalled) {
             await window.ethereum.send({
                 method: "wallet_enable",
                 params: [{
-                    [pluginOrigin]: {}
+                    [origin ? origin : defaultPluginOrigin]: {}
                 }]
             });
-            //TODO
-            await addAsset();
+            addKusamaAsset();
         }
         return this;
     }
 }
 
-export function injectMetamaskPolkadotSnapProvider(network: "kusama"|"polkadot", config: NetworkConfig): void {
+export function injectMetamaskPolkadotSnapProvider(
+    network: "kusama"|"polkadot", 
+    config: NetworkConfig,
+    pluginOrigin?: string
+    ): void {
     const PolkadotSnap = new MetamaskPolkadotSnap(network, config);
     if(isPolkadotSnapInstalled && installPolkadotSnap) {
-    injectExtension(PolkadotSnap.enableSnap(network), { name: 'metmask-polkadot-snap', version: '1.0.0' });
+    injectExtension(() => PolkadotSnap.enableSnap(pluginOrigin), { name: 'metmask-polkadot-snap', version: '1.0.0' });
     }   
  }
+
+declare global {
+    interface Window {
+        ethereum: {
+            isMetaMask: boolean;
+            send: (params: any)=> Promise<any>;
+            on: (eventName: any, callback: any) => any;
+        }
+    }
+}
+export const origin = new URL('package.json', 'http://localhost:8081').toString();
+export const pluginOrigin = `wallet_plugin_${origin}`;
+
+export async function getAccountAddress(): Promise<string> {
+    try {
+        const result: any = await window.ethereum.send({
+            method: 'getAddress',
+        });
+        return result;
+    } catch (e) {
+        console.log(e);
+        return "";
+    }
+}
+
+export async function installPolkadotSnap(): Promise<boolean> {
+    try {
+        await window.ethereum.send({
+            method: 'wallet_enable',
+            params: [{
+                [pluginOrigin]: {}
+            }]
+        });
+        console.log("Snap installed!!");
+        return true;
+    } catch (e) {
+        console.log(e);
+        return false;
+    }
+}
+
+export async function isPolkadotSnapInstalled(): Promise<boolean> {
+    try {
+        const result: any = await window.ethereum.send({
+            method: 'wallet_getPlugins',
+        });
+        return !!result[origin];
+    } catch (e) {
+        console.log(e);
+        return false;
+    }
+}
+
+export async function addKusamaAsset(): Promise<void> {
+    return await window.ethereum.send({
+        method: pluginOrigin,
+        params: [{
+            method: 'addKusamaAsset'
+        }]
+    });
+}
