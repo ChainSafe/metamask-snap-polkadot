a7b97c91f74bf8e6960c378486e40f6800278eed
irubido <ivan.rubido@gmail.com>
2020-04-15 15:27:54 +0200

    getMetamaskSnapApi method

--- adapter/snap.ts
+++ adapter/snap.ts
@@ -1,11 +1,24 @@
 import {Injected, InjectedAccounts, InjectedAccount} from "@polkadot/extension-inject/types";
 import { Signer as InjectedSigner } from '@polkadot/api/types';
-import {getAccountAddress, isPolkadotSnapInstalled, addKusamaAsset} from "./methods"
+import {getAccountAddress, isPolkadotSnapInstalled, addKusamaAsset, 
+    addPolkadotAsset, getBalance, getAddress, getPublicKey, exportSeed, installPolkadotSnap} from "./methods";
 
 export interface NetworkConfig {
     explorerUrl: string;
     rpcUrl: string;
     unitName: string;
+}
+
+export interface MetamaskSnapApi {
+    getAccountAddress(): Promise<string>;
+    installPolkadotSnap(): Promise<boolean>;
+    isPolkadotSnapInstalled(): Promise<boolean>;
+    addKusamaAsset(): Promise<void>;
+    addPolkadotAsset(): Promise<void>;
+    getBalance(): Promise<string>;
+    getAddress(): Promise<string>;
+    getPublicKey(): Promise<string>;
+    exportSeed(): Promise<string>;
 }
 
 export class MetamaskPolkadotSnap implements Injected {
@@ -45,5 +58,19 @@ export class MetamaskPolkadotSnap implements Injected {
             addKusamaAsset();
         }
         return this;
+    }
+
+    public getMetamaskSnapApi = async (type: string): Promise<MetamaskSnapApi> => {
+        return {
+            getAccountAddress,
+            installPolkadotSnap,
+            isPolkadotSnapInstalled,
+            addKusamaAsset,
+            addPolkadotAsset,
+            getBalance,
+            getAddress,
+            getPublicKey,
+            exportSeed
+        }
     }
 }
