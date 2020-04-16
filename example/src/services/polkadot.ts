017509dd996fa456a11df6e71186c34a442e90ec
mak <mak@nodefactory.io>
2020-04-16 15:47:53 +0200

    Remove console logging and rename function inside polkadotEmitter

--- example/src/services/polkadot.ts
+++ example/src/services/polkadot.ts
@@ -5,15 +5,12 @@ let api: PolkadotApi;
 
 export async function getPolkadotApi(): Promise<PolkadotApi|undefined> {
     if (!api) {
-        console.log("Initializing polkadot API");
         try {
             let index = await window.ethereum.requestIndex();
             api = await index.getPluginApi(origin);
         } catch (e) {
             console.log(e);
         }
-    } else {
-        console.log("Request for polkadot API");
     }
     return api;
 }
