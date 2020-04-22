2d4c826de43cc4fa675266c4474e731a22af0dbc
mak <mak@nodefactory.io>
2020-04-22 09:43:47 +0200

    Refactor sendUnit function to return hash

--- packages/snap/src/rpc/substrate/sendUnit.ts
+++ packages/snap/src/rpc/substrate/sendUnit.ts
@@ -1,19 +1,11 @@
 import ApiPromise from "@polkadot/api/promise";
 import {Wallet} from "../../interfaces";
-import { Keyring } from "@polkadot/api";
-import { Hash } from '@polkadot/types/interfaces/runtime';
+import {Hash} from '@polkadot/types/interfaces/runtime';
+import {getKeyPair} from "../../polkadot/account";
 
 export async function sendUnit(wallet: Wallet, api: ApiPromise, amount: string|number, to: string): Promise<Hash> {
-  const keyring = new Keyring();
-  const keypair = keyring.addFromJson(wallet.getPluginState().polkadot.account.keyring);
-  const { nonce } = await api.query.system.account(keypair.address);
-  const transfer = api.tx.balances.transfer(to, amount);
-  return new Promise((resolve) => {
-    transfer.signAndSend(keypair, {nonce}, ({ events = [], status }) => {
-      if (status.isInBlock) {
-        console.log('Included at block hash', status.asInBlock.toHex());
-        resolve(status.asInBlock);
-      }
-    });
-  });
+  const keypair = await getKeyPair(wallet);
+  return await api.tx.balances
+    .transfer(to, amount)
+    .signAndSend(keypair);
 }
