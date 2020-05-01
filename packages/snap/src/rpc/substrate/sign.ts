545cc4f5ddf53013db89209b6dd4a03916201113
Marin Petrunić <marin.petrunic@gmail.com>
2020-05-01 14:39:22 +0200

    add snap sign confirmation

--- packages/snap/src/rpc/substrate/sign.ts
+++ packages/snap/src/rpc/substrate/sign.ts
@@ -3,6 +3,7 @@ import ApiPromise from "@polkadot/api/promise";
 import { SignerPayloadJSON, SignerPayloadRaw } from '@polkadot/types/types';
 import {getKeyPair} from "../../polkadot/account";
 import {hexToU8a, u8aToHex} from "@polkadot/util";
+import {showConfirmationDialog} from "../../util/confirmation";
 
 export async function signPayloadJSON(
   wallet: Wallet, api: ApiPromise, payload: SignerPayloadJSON
@@ -14,10 +15,18 @@ export async function signPayloadJSON(
 
 export async function signPayloadRaw(
   wallet: Wallet, api: ApiPromise, payload: SignerPayloadRaw
-): Promise<{ signature: string }> {
-  const keyPair = await getKeyPair(wallet);
-  const signedBytes = keyPair.sign(hexToU8a(payload.data));
-  return {
-    signature: u8aToHex(signedBytes)
-  };
+): Promise<{ signature: string }|void> {
+  // ask for confirmation
+  const confirmation = await showConfirmationDialog(
+    wallet,
+    `Do you want to sign following message: \n "${payload.data}" \n with account ${payload.address}`
+  );
+  // return seed if user confirmed action
+  if (confirmation) {
+    const keyPair = await getKeyPair(wallet);
+    const signedBytes = keyPair.sign(hexToU8a(payload.data));
+    return {
+      signature: u8aToHex(signedBytes)
+    };
+  }
 }
