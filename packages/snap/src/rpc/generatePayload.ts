c223aa15fb7668481a3f36c965105c30fa803532
mak <mak@nodefactory.io>
2020-05-04 18:31:49 +0200

    Implement generatePayload

--- packages/snap/src/rpc/generatePayload.ts
+++ packages/snap/src/rpc/generatePayload.ts
@@ -1,8 +1,33 @@
 import ApiPromise from "@polkadot/api/promise";
 import {SignerPayloadJSON} from '@polkadot/types/types';
 import {Wallet} from "../interfaces";
+import {getAddress} from "./getAddress";
+import {SubmittableExtrinsic} from "@polkadot/api/types";
 
 export async function generatePayload(wallet: Wallet, api: ApiPromise, to: string, amount: string | number): Promise<SignerPayloadJSON> {
-    const signerPayload = api.registry.createType('SignerPayload', {amount, to});
+    // fetch last signed block and account address
+    const [signedBlock, address] = await Promise.all([api.rpc.chain.getBlock(), getAddress(wallet)]);
+    // create signer options
+    const nonce = (await api.derive.balances.account(address)).accountNonce;
+    const signerOptions = {
+        blockHash: signedBlock.block.header.hash,
+        era: api.createType('ExtrinsicEra', {
+            current: signedBlock.block.header.number,
+            period: 50
+        }),
+        nonce
+    };
+    // define transaction method
+    const transaction: SubmittableExtrinsic<'promise'> = api.tx.balances.transfer(to, amount);
+    // create SignerPayload
+    const signerPayload = api.createType('SignerPayload', {
+        genesisHash: api.genesisHash,
+        runtimeVersion: api.runtimeVersion,
+        version: api.extrinsicVersion,
+        ...signerOptions,
+        address: to,
+        blockNumber: signedBlock.block.header.number,
+        method: transaction.method
+    });
     return signerPayload.toPayload();
 }
