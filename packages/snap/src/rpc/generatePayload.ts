ab660ef7d4e39bc5c723603aef930d8759345cb1
mak <mak@nodefactory.io>
2020-05-04 17:52:59 +0200

    Expand index for easy testing

--- packages/snap/src/rpc/generatePayload.ts
+++ packages/snap/src/rpc/generatePayload.ts
@@ -3,6 +3,6 @@ import {SignerPayloadJSON} from '@polkadot/types/types';
 import {Wallet} from "../interfaces";
 
 export async function generatePayload(wallet: Wallet, api: ApiPromise, to: string, amount: string | number): Promise<SignerPayloadJSON> {
-    let payload = api.registry.createType('SignerPayload', {amount, to});
-    return payload.toPayload();
+    const signerPayload = api.registry.createType('SignerPayload', {amount, to});
+    return signerPayload.toPayload();
 }
