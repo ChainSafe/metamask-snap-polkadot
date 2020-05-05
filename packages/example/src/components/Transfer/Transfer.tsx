2bc911f45def00610d3d978ec5cd5030a51db792
mak <mak@nodefactory.io>
2020-05-05 17:44:46 +0200

    Add chips for transaction state

--- packages/example/src/components/Transfer/Transfer.tsx
+++ packages/example/src/components/Transfer/Transfer.tsx
@@ -1,5 +1,17 @@
 import React, {useCallback, useState} from "react";
-import {Box, Button, Card, CardContent, CardHeader, Grid, TextField, InputAdornment} from '@material-ui/core/';
+import {
+    Box,
+    Button,
+    Card,
+    CardContent,
+    CardHeader,
+    Grid,
+    TextField,
+    InputAdornment,
+    Hidden,
+    Badge,
+    Chip
+} from '@material-ui/core/';
 import {getInjectedMetamaskExtension} from "../../services/metamask";
 
 interface ITransferProps {
@@ -9,6 +21,8 @@ interface ITransferProps {
 export const Transfer: React.FC<ITransferProps> = ({network}) => {
     const [recipient, setRecipient] = useState<string>("");
     const [amount, setAmount] = useState<string | number>("");
+    const [included, setIncluded] = useState(true);
+    const [finalized, setFinalized] = useState(true);
 
     const handleCurrency = (network: string): string => {
         switch(network) {
@@ -26,6 +40,14 @@ export const Transfer: React.FC<ITransferProps> = ({network}) => {
         setAmount(event.target.value);
     }, [setAmount]);
 
+    const handleTransactionIncluded = useCallback(() => {
+        setIncluded(true);
+    }, [setIncluded]);
+
+    const handleTransactionFinalized = useCallback(() => {
+        setFinalized(true);
+    }, [setFinalized]);
+
     const onSubmit = async () => {
         const provider = await getInjectedMetamaskExtension();
         if(provider && provider.signer.signPayload) {
@@ -33,6 +55,9 @@ export const Transfer: React.FC<ITransferProps> = ({network}) => {
             const txPayload = await api.generateTransactionPayload(amount, recipient);
             const signedTx = await provider.signer.signPayload(txPayload.payload);
             let txHash = await api.send(signedTx.signature, txPayload);
+            // subscribe to transaction events
+            const polkadotEventApi = await api.getEventApi();
+            polkadotEventApi.subscribeToTxStatus(txHash, handleTransactionIncluded, handleTransactionFinalized)
         }
     };
 
@@ -54,6 +79,18 @@ export const Transfer: React.FC<ITransferProps> = ({network}) => {
                 <Box m="0.5rem"/>
                 <Grid container item xs={12} justify="flex-end">
                     <Button onClick={onSubmit} color="secondary" variant="contained" size="large">SEND</Button>
+                </Grid>
+                <Grid container spacing={1}>
+                    <Hidden xsUp={!included}>
+                        <Grid item>
+                            <Chip color={"primary"} label="Included" />
+                        </Grid>
+                    </Hidden>
+                    <Hidden xsUp={!finalized}>
+                        <Grid item>
+                            <Chip color={"primary"} label="Finalized" />
+                        </Grid>
+                    </Hidden>
                 </Grid>
             </CardContent>
         </Card>
