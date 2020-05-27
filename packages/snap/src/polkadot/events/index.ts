f269b2dc607b19db6110d422cfe2645d3cfac76c
mak <mak@nodefactory.io>
2020-05-27 10:48:11 +0200

    Fix linter errors

--- packages/snap/src/polkadot/events/index.ts
+++ packages/snap/src/polkadot/events/index.ts
@@ -6,24 +6,24 @@ import StrictEventEmitter from "strict-event-emitter-types";
 // Polkadot event emitters
 
 interface PolkadotEvents {
-    onBalanceChange: string;
+  onBalanceChange: string;
 }
 
 const polkadotEventsEmitterProvider = new StrictEmitterWithOriginProvider<PolkadotEvents>();
 
 export function getPolkadotEventEmitter(origin: string): StrictEventEmitter<EventEmitter, PolkadotEvents> {
-    return polkadotEventsEmitterProvider.getEventEmitter(origin);
+  return polkadotEventsEmitterProvider.getEventEmitter(origin);
 }
 
 // Transaction event emitters
 
 interface TxEvents {
-    included: TxStatus;
-    finalized: TxStatus;
+  included: TxStatus;
+  finalized: TxStatus;
 }
 
 const txEventsEmitterProvider = new StrictEmitterWithOriginProvider<TxEvents>();
 
 export function getTxEventEmitter(hash: string): StrictEventEmitter<EventEmitter, TxEvents> {
-    return txEventsEmitterProvider.getEventEmitter(hash);
+  return txEventsEmitterProvider.getEventEmitter(hash);
 }
