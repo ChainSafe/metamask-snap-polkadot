2b29ea9955363e13271eeecfc90d0c0a21f3df63
Mak Muftic <mak@nodefactory.io>
2020-07-14 16:29:13 +0200

    Fix linter errors

--- packages/snap/src/rpc/send.ts
+++ packages/snap/src/rpc/send.ts
@@ -5,40 +5,42 @@ import {getAddress} from "./getAddress";
 import {getTxEventEmitter} from "../polkadot/events";
 import {saveTxToState, updateTxInState} from "../polkadot/tx";
 
-export async function send(wallet: Wallet, api: ApiPromise, signature: string, txPayload: TxPayload): Promise<Transaction> {
-    const sender = await getAddress(wallet);
-    const destination = txPayload.payload.address;
+export async function send(
+  wallet: Wallet, api: ApiPromise, signature: string, txPayload: TxPayload
+): Promise<Transaction> {
+  const sender = await getAddress(wallet);
+  const destination = txPayload.payload.address;
 
-    const extrinsic = api.createType('Extrinsic', txPayload.tx);
-    extrinsic.addSignature(sender, signature, txPayload.payload);
-    const txHash = extrinsic.hash.toHex();
+  const extrinsic = api.createType('Extrinsic', txPayload.tx);
+  extrinsic.addSignature(sender, signature, txPayload.payload);
+  const txHash = extrinsic.hash.toHex();
 
-    const amount = extrinsic.args[1].toJSON();
-    const paymentInfo = await api.tx.balances
-        .transfer(destination, Number(amount.toString()))
-        .paymentInfo(sender);
-    const tx = {
-        sender: sender,
-        destination: destination,
-        block: "",
-        hash: extrinsic.hash.toHex(),
-        amount: amount,
-        fee: paymentInfo.partialFee.toJSON()
-    } as Transaction;
+  const amount = extrinsic.args[1].toJSON();
+  const paymentInfo = await api.tx.balances
+    .transfer(destination, Number(amount.toString()))
+    .paymentInfo(sender);
+  const tx = {
+    amount: amount,
+    block: "",
+    destination: destination,
+    fee: paymentInfo.partialFee.toJSON(),
+    hash: extrinsic.hash.toHex(),
+    sender: sender,
+  } as Transaction;
 
-    api.rpc.author.submitAndWatchExtrinsic(extrinsic, result => {
-        const eventEmitter = getTxEventEmitter(txHash);
-        if (result.isInBlock) {
-            tx.block = result.hash.toHex();
-            updateTxInState(wallet, tx);
-            eventEmitter.emit("included", {txHash});
-            eventEmitter.removeAllListeners("included");
-        } else if (result.isFinalized) {
-            eventEmitter.emit("finalized", {txHash});
-            eventEmitter.removeAllListeners("finalized");
-        }
-    });
+  api.rpc.author.submitAndWatchExtrinsic(extrinsic, result => {
+    const eventEmitter = getTxEventEmitter(txHash);
+    if (result.isInBlock) {
+      tx.block = result.hash.toHex();
+      updateTxInState(wallet, tx);
+      eventEmitter.emit("included", {txHash});
+      eventEmitter.removeAllListeners("included");
+    } else if (result.isFinalized) {
+      eventEmitter.emit("finalized", {txHash});
+      eventEmitter.removeAllListeners("finalized");
+    }
+  });
 
-    saveTxToState(wallet, tx);
-    return tx;
+  saveTxToState(wallet, tx);
+  return tx;
 }
