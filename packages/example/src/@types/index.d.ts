aceb356517dd5c9bf35f466c2a065e635208a960
irubido <ivan.rubido@gmail.com>
2022-06-09 16:17:16 +0200

    injectMetamaskPolkadotSnapProvider type for lint fix

---
+++ packages/example/src/@types/index.d.ts
@@ -0,0 +1,8 @@
+import { SnapConfig } from "@chainsafe/metamask-polkadot-types";
+
+declare module "@chainsafe/metamask-polkadot-adapter" {
+
+  export function injectMetamaskPolkadotSnapProvider(network: "westend" | "kusama",
+    config?: SnapConfig,
+    pluginOrigin?: string): void;
+}
