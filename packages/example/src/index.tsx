bd03207836e7bac633fd5bc564f9b4fd6f22b85c
Anton Lykhoyda <lykhoyda@gmail.com>
2023-08-14 15:21:15 +0200

    update

--- packages/example/src/index.tsx
+++ packages/example/src/index.tsx
@@ -7,6 +7,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
 
 root.render(
