6055bde87f52bbbf2e99d6ebe91238b779100a3b
Anton Lykhoyda <lykhoyda@gmail.com>
2023-08-18 17:22:37 +0200

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  extends: '@chainsafe',
+  extends: '@chainsafe/eslint-config/ts-mocha-chai',
   rules: {
     'import/no-extraneous-dependencies': 'warn'
   }
