d85f3168399dde0f228530a4df687d51acb35826
irubido <ivan.rubido@gmail.com>
2023-10-09 13:00:32 +0200

    snaps cli 3

--- packages/snap/src/polkadot/account.ts
+++ packages/snap/src/polkadot/account.ts
@@ -1,7 +1,6 @@
 import type { KeyringPair } from '@polkadot/keyring/types';
 import { Keyring } from '@polkadot/keyring';
 import { stringToU8a } from '@polkadot/util';
-import type { JsonBIP44CoinTypeNode } from '@metamask/key-tree';
 import type { SnapNetworks } from '@chainsafe/metamask-polkadot-types';
 import { getConfiguration } from '../configuration';
 
@@ -11,12 +10,12 @@ import { getConfiguration } from '../configuration';
 export async function getKeyPair(): Promise<KeyringPair> {
   const config = await getConfiguration();
 
-  const bip44Node = (await snap.request({
+  const bip44Node = await snap.request({
     method: 'snap_getBip44Entropy',
     params: {
       coinType: getCoinTypeByNetwork(config.networkName)
     }
-  })) as JsonBIP44CoinTypeNode;
+  });
 
   // generate keys
   const seed = bip44Node.privateKey.slice(0, 32);
