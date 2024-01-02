68376ca72166a6d22ee141cafcd8412d6b56dd68
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-01-02 10:28:25 -0500

    fix transfer

--- packages/snap/src/rpc/generateTransactionPayload.ts
+++ packages/snap/src/rpc/generateTransactionPayload.ts
@@ -21,7 +21,10 @@ export async function generateTransactionPayload(
     nonce
   };
   // define transaction method
-  const transaction: SubmittableExtrinsic<'promise'> = api.tx.balances.transfer(to, amount);
+  const transaction: SubmittableExtrinsic<'promise'> = api.tx.balances.transferAllowDeath(
+    to,
+    amount
+  );
 
   // create SignerPayload
   const signerPayload = api.createType('SignerPayload', {
