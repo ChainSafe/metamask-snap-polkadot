45d6452b65e167bedf94933ed34c78760f9ae6ea
irubido <ivan.rubido@gmail.com>
2024-06-18 12:55:33 +0200

    type to number

--- packages/snap/src/polkadot/account.ts
+++ packages/snap/src/polkadot/account.ts
@@ -26,7 +26,7 @@ export async function getKeyPair(): Promise<KeyringPair> {
   return keyring.addFromSeed(stringToU8a(seed));
 }
 
-export const getCoinTypeByNetwork = (network: SnapNetworks): 434 | 354 => {
+export const getCoinTypeByNetwork = (network: SnapNetworks): number => {
   switch (network) {
     case 'kusama':
     case 'westend':
