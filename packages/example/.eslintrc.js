fad373a35fc65f1ab94ae57e7ccfc31a0d881a01
Anton Lykhoyda <lykhoyda@gmail.com>
2024-08-21 18:09:21 +0200

    update node

--- packages/example/.eslintrc.js
+++ packages/example/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-unsafe-return': 'warn',
     '@typescript-eslint/no-unsafe-assignment': 'warn',
-    '@typescript-eslint/no-misused-promises': 'off'
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'warn'
   }
 };
