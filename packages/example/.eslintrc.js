812085b7f65d821706b7441f4d4644cf25b57ac8
Anton Lykhoyda <lykhoyda@gmail.com>
2024-08-26 17:24:11 +0200

    update libs

--- packages/example/.eslintrc.js
+++ packages/example/.eslintrc.js
@@ -8,8 +8,9 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/no-unsafe-return': 'warn',
+    "@typescript-eslint/no-unsafe-call": 'warn',
     '@typescript-eslint/no-unsafe-assignment': 'warn',
     '@typescript-eslint/no-misused-promises': 'off',
-    '@typescript-eslint/no-unsafe-member-access': 'warn'
+    '@typescript-eslint/no-unsafe-member-access': 'warn',
   }
 };
